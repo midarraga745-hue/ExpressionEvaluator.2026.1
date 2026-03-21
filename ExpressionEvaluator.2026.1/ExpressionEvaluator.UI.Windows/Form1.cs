de0140d43e933cb9046febbaa9e2500b6345db40
MAR�A STELLA IDARRAGA RAMIREZ <mariaidarraga1163747@correo.itm.edu.co>
2026-03-21 14:04:09 -0500

    Agregar interfaz de usuario Windows Forms

---
+++ ExpressionEvaluator.2026.1/ExpressionEvaluator.UI.Windows/Form1.cs
@@ -0,0 +1,10 @@
+namespace ExpressionEvaluator.UI.Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
